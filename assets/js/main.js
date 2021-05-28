ee0f0d2ff3228fcb372e62d16f84263b3116801c
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-05-28 15:17:01 +0200

    Caricati dati sulla pagina

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        dischi: [],
+    },
+    methods: {
+
+    },
+
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then(response => {
+                this.dischi.push(response.data.response)
+                console.log(this.dischi);
+            })
+
+
+
+    },
+})
+
+
+/*
+Attraverso una chiamata ajax all’API di boolean https://flynn.boolean.careers/exercises/api/array/music
+avremo a disposizione una decina di dischi musicali.
+Utilizzando vue, stampiamo a schermo una card per ogni album.
+
+
+*/
